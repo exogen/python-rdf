c238e03f9217d8b3eaa0d681b3fbaefca607e824
Brian Beck <exogen@gmail.com>
2010-06-23 03:50:40 -0400

    run_testcases.py will now hijack argv to allow running test cases by URI.

--- tests/run_testcases.py
+++ tests/run_testcases.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import sys
 import unittest
 
 from rdf.namespace import TEST
@@ -7,11 +8,26 @@ from rdf.testcases.unittest import RDFTestSuite
 from util import open_data_file, TEST_OPENER
 
 
+TEST_URIS = set()
+
 def testcases():
     manifest = Manifest(open_data_file('rdfcore/Manifest.rdf'))
-    suite = unittest.TestSuite()
-    return RDFTestSuite.from_manifest(manifest, opener=TEST_OPENER)
+    rdf_suite = RDFTestSuite.from_manifest(manifest, opener=TEST_OPENER)
+    if not TEST_URIS:
+        return rdf_suite
+    else:
+        suite = RDFTestSuite()
+        for test in rdf_suite:
+            if test.id() in TEST_URIS:
+                suite.addTest(test)
+        return suite
 
 if __name__ == '__main__':
-    unittest.main(defaultTest='testcases')
+    argv = sys.argv[:1]
+    for arg in sys.argv[1:]:
+        if arg.startswith('-'):
+            argv.append(arg)
+        else:
+            TEST_URIS.add(arg)
+    unittest.main(defaultTest='testcases', argv=argv)
 
